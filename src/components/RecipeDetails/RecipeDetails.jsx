be719dda913061b643af363d644187983c37ddb2
Feizo <felixkebaki@gmail.com>
2024-09-04 10:12:11 +0300

    Recipe Details

--- src/components/RecipeDetails/RecipeDetails.jsx
+++ src/components/RecipeDetails/RecipeDetails.jsx
@@ -3,6 +3,7 @@ import "./recipeDetails.css";
 import { Link, useParams } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircle } from "@fortawesome/free-solid-svg-icons";
+import { faUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import { GlobalFetchContext } from "../../context/GlobalContext";
 
 export function RecipeDetails() {
@@ -17,7 +18,7 @@ export function RecipeDetails() {
     if (data && data.meals) {
       setRecipeDetails(data.meals);
     }
-    console.log(recipeDetails);
+    
   }
 
   useEffect(() => {
@@ -27,57 +28,77 @@ export function RecipeDetails() {
     <section className="RecipeDetailsMainSec">
       {recipeDetails &&
         recipeDetails.map((details) => (
-          <div key={details.idMeal} className="EachClassDetailsRe">
-            <div className="DetailsImageWrapper">
-              <img src={details.strMealThumb} alt="" />
-            </div>
-            <div className="DetailsWordwrapperEach">
-              <div className="InsideDetailsWording">
-                <p id="EachRecipeName">{details.strMeal}</p>
-                <p id="IngredTittleEach">Ingredients</p>
-                <p id="AllIngredientsEach">
-                  {details.strIngredient1}: <span>{details.strMeasure1}</span>,
-                  {details.strIngredient2}: <span>{details.strMeasure2}</span>,
-                  {details.strIngredient3}: <span>{details.strMeasure3}</span>
-                  {details.strIngredient4}: <span>{details.strMeasure4}</span>,
-                  {details.strIngredient5}: <span>{details.strMeasure5}</span>,
-                  {details.strIngredient6}: <span>{details.strMeasure6}</span>
-                  {details.strIngredient7}: <span>{details.strMeasure7}</span>,
-                  {details.strIngredient8}: <span>{details.strMeasure8}</span>,
-                  {details.strIngredient9}: <span>{details.strMeasure9}</span>
-                  {details.strIngredient10}: <span>{details.strMeasure10}</span>
-                  ,{details.strIngredient11}:{" "}
-                  <span>{details.strMeasure11}</span>,{details.strIngredient12}:{" "}
-                  <span>{details.strMeasure12}</span>
-                  {details.strIngredient13}: <span>{details.strMeasure13}</span>
-                  ,{details.strIngredient14}:{" "}
-                  <span>{details.strMeasure14}</span>,{details.strIngredient15}:{" "}
-                  <span>{details.strMeasure15}</span>
-                </p>
-                <div className="ButtonDivWrapperDetails">
-                <Link id="ShopAtEacDetails">Shop Ingredients</Link>
-                </div>
-                <p id="InstructionTittle">Instructions</p>
-                <p id="AllInstructionsEach">{details.strInstructions}</p>
-                <div className="WrapperTagsInDetails">
-                  {details.strArea && (
-                    <div>
-                      <FontAwesomeIcon icon={faCircle} id="DetailsDotEach"/>
-                      <p>{details.strArea}</p>
-                    </div>
-                  )}
-                  {details.strCategory && (
-                    <div>
-                      <FontAwesomeIcon icon={faCircle} id="DetailsDotEach"/>
-                      <p>{details.strCategory}</p>
-                    </div>
-                  )}
-                  {details.strTags && (
-                    <div>
-                      <FontAwesomeIcon icon={faCircle} id="DetailsDotEach"/>
-                      <p>{details.strTags}</p>
-                    </div>
-                  )}
+          <div key={details.idMeal}>
+            <div className="EachClassDetailsRe">
+              <div className="DetailsImageWrapper">
+                <img src={details.strMealThumb} alt="" />
+              </div>
+              <div className="DetailsWordwrapperEach">
+                <div className="InsideDetailsWording">
+                  <p id="EachRecipeName">{details.strMeal}</p>
+                  <p id="IngredTittleEach">Ingredients</p>
+                  <p id="AllIngredientsEach">
+                    {details.strMeasure1 &&
+                      `${details.strMeasure1} ${details.strIngredient1}  `}
+                    {details.strMeasure2 &&
+                      `,${details.strMeasure2} ${details.strIngredient2}  `}
+                    {details.strMeasure3 &&
+                      `,${details.strMeasure3} ${details.strIngredient3}  `}
+                    {details.strMeasure4 &&
+                      `,${details.strMeasure4} ${details.strIngredient4}  `}
+                    {details.strMeasure5 &&
+                      `,${details.strMeasure5} ${details.strIngredient5}  `}
+                    {details.strMeasure6 &&
+                      `,${details.strMeasure6} ${details.strIngredient6}  `}
+                    {details.strMeasure7 &&
+                      `,${details.strMeasure7} ${details.strIngredient7}  `}
+                    {details.strMeasure8 &&
+                      `,${details.strMeasure8} ${details.strIngredient8}  `}
+                    {details.strMeasure9 &&
+                      `,${details.strMeasure9} ${details.strIngredient9}  `}
+                    {details.strMeasure10 &&
+                      `,${details.strMeasure10} ${details.strIngredient10}  `}
+                    {details.strMeasure11 &&
+                      `,${details.strMeasure11} ${details.strIngredient11}  `}
+                    {details.strMeasure12 &&
+                      `,${details.strMeasure12} ${details.strIngredient12}  `}
+                    {details.strMeasure13 &&
+                      `,${details.strMeasure13} ${details.strIngredient13}  `}
+                    {details.strMeasure14 &&
+                      `,${details.strMeasure14} ${details.strIngredient14}  `}
+                    {details.strMeasure15 &&
+                      `,${details.strMeasure15} ${details.strIngredient15}  `}
+                  </p>
+                  <div className="ButtonDivWrapperDetails">
+                    <Link id="ShopAtEacDetails">Shop Ingredients</Link>
+                  </div>
+                  <p id="InstructionTittle">Instructions</p>
+                  <p id="AllInstructionsEach">{details.strInstructions}</p>
+                  {details.strSource &&
+                  <a href={details.strSource} className="MoreToNewWebsite">
+                  <p id="MoreToNewWebsite" target="_blank">More details</p>
+                  <FontAwesomeIcon icon={faUpRightFromSquare} id="MoreIconLinkRecipe"/>
+                  </a>}
+                  <div className="WrapperTagsInDetails">
+                    {details.strArea && (
+                      <div>
+                        <FontAwesomeIcon icon={faCircle} id="DetailsDotEach" />
+                        <p>{details.strArea}</p>
+                      </div>
+                    )}
+                    {details.strCategory && (
+                      <div>
+                        <FontAwesomeIcon icon={faCircle} id="DetailsDotEach" />
+                        <p>{details.strCategory}</p>
+                      </div>
+                    )}
+                    {details.strTags && (
+                      <div>
+                        <FontAwesomeIcon icon={faCircle} id="DetailsDotEach" />
+                        <p>{details.strTags}</p>
+                      </div>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
