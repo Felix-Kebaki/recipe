2d96efaf654cb5bd656b8a3f44fda39bc564e603
Feizo <felixkebaki@gmail.com>
2024-09-11 12:37:29 +0300

    FAQs responsive 2

--- src/components/Testimonials/Testimonial.jsx
+++ src/components/Testimonials/Testimonial.jsx
@@ -12,31 +12,31 @@ export function Testimonial() {
           <div className="ImageOneDivTesti">
             <img src={Family1} id="TestimonialPics" />
           </div>
-          <div>
+          <div className="ActualTestimonial1">
             <p id="ActualTestimonial">
               These recipes made cooking so easy!My family loves every
               dish.Highly recommend for busy weeknights!
             </p>
           </div>
-          <div>
+          <div className="ActualTestimonial2">
             <p id="ActualTestimonial">
               Incredible flavors and simple instructions.I’ve tried three
               recipes so far and each one turned out amazing!
             </p>
           </div>
-          <div>
+          <div className="ActualTestimonial3">
             <p id="ActualTestimonial">
               I never knew cooking could be this fun!Your recipes have
               transformed my meals into something special.
             </p>
           </div>
-          <div>
+          <div className="ActualTestimonial4">
             <p id="ActualTestimonial">
               The ingredients are so fresh and the recipes are foolproof.My
               confidence in the kitchen has skyrocketed!
             </p>
           </div>
-          <div>
+          <div className="ActualTestimonial5">
             <p id="ActualTestimonial">
               Tried the pasta recipe and it was a hit!Easy,quick and absolutely
               delicious.Will order again!
